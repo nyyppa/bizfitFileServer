8385dd8a4ac5f046411c494cd494efdebb0a094e
atte yliverronen <atte.yliverronen@gmail.com>
2017-08-09 07:36:59 +0000

    moo

--- bizfitserver/workspace/emali.js
+++ bizfitserver/workspace/emali.js
@@ -15,7 +15,7 @@ var mailListener = new MailListener({
   debug: console.log, // Or your custom function with only one incoming argument. Default: null 
   tlsOptions: { rejectUnauthorized: false },
   mailbox: "INBOX", // mailbox to monitor 
-  searchFilter: ["SEEN"], // the search filter being used after an IDLE notification has been retrieved 
+  searchFilter: ["UNSEEN"], // the search filter being used after an IDLE notification has been retrieved 
   markSeen: true, // all fetched email willbe marked as seen and not fetched next time 
   fetchUnreadOnStart: true, // use it only if you want to get all unread email on lib start. Default is `false`, 
   mailParserOptions: {streamAttachments: true}, // options to be passed to mailParser lib. 
